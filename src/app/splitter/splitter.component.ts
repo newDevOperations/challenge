947c9567913046451e5d449b53fa1593494da4fa
unknown <denis.lagnier@gmail.com>
2022-11-17 01:36:17 +0100

    challenge v1.0

--- src/app/splitter/splitter.component.ts
+++ src/app/splitter/splitter.component.ts
@@ -4,12 +4,9 @@ import { Participant } from "../classes/participant";
 @Component({
   selector: "app-splitter",
   templateUrl: `splitter.component.html`,
-  styleUrls: [
-    '../shared/message-head.component.css'
-  ]
+  styleUrls: ["../shared/message-head.component.css"],
 })
 export class SplitterComponent implements OnInit {
-  
   importDatas!: string;
   importDate!: string;
 
@@ -22,19 +19,16 @@ export class SplitterComponent implements OnInit {
   totalJackpot: number = 0;
   jackpotList: Array<number> = [0, 0, 0];
 
-  
   constructor() {}
 
   ngOnInit(): void {
     //set to Sunday of last 2 weeks
     let d = new Date();
-    d.setDate(d.getDate() - (d.getDay() + 6) % 7 - 7);
-    this.importDate = new Date(d).toISOString().split('T')[0];
+    d.setDate(d.getDate() - ((d.getDay() + 6) % 7) - 7);
+    this.importDate = new Date(d).toISOString().split("T")[0];
   }
 
-  
   onSubmitImportForm() {
-  
     const finalRepartition = [0.5, 0.3, 0.2];
     const regex = new RegExp(
       "^(.+)\\s+:?([0-9]+)(\\s*\\(\\s?auto\\s?\\){1})?$"
@@ -42,7 +36,7 @@ export class SplitterComponent implements OnInit {
 
     let elements = this.importDatas.trim().split("\n");
     this.participantsList = [];
-    
+
     // import datas into participantsList
     for (let element of elements) {
       let line = regex.exec(element.trim());
@@ -88,11 +82,9 @@ export class SplitterComponent implements OnInit {
     //adjust last value to repair round problems
     this.jackpotList[2] =
       this.totalJackpot - this.jackpotList[0] - this.jackpotList[1];
-
   }
 
   onSubmitConfigForm(data: any) {
-
     let emptyParticipant = new Participant("Empty", 0, false);
     let participantsListCopy = [...this.participantsList];
 
@@ -101,7 +93,7 @@ export class SplitterComponent implements OnInit {
         .sort((a, b) => b.donation - a.donation)
         .map((key) => [key.name, key])
     );
-  
+
     this.winnersList = [
       workList.get(data.winnerGold) ?? emptyParticipant,
       workList.get(data.winnerSilver) ?? emptyParticipant,
@@ -119,7 +111,7 @@ export class SplitterComponent implements OnInit {
     workList.delete(data.winnerBronze);
     workList.delete(data.dispatcher);
 
-     //repartition
+    //repartition
     let sum = 0;
     for (let n = 0; n <= 2; n++) {
       sum = this.repartitionsList[n][0].donation;
@@ -131,7 +123,7 @@ export class SplitterComponent implements OnInit {
           workList.delete(cle);
         }
       }
-      
+
       //add dispatcher
       this.repartitionsList[n].push(
         new Participant(
@@ -140,13 +132,13 @@ export class SplitterComponent implements OnInit {
           this.dispatcher.isAuto
         )
       );
-     
     }
     this.participantsList = [...participantsListCopy];
   }
 
-  onSubmitTestImportForm(){
-    this.importDatas = "Promesses de dons (Nom suivi de pf, suivi de '(auto)' si vous voulez un report automatique):\n\nMagicHannibal 40 (auto)\nBelli 1er le croquant 40\nbullitt666 30 (auto)\nSamneveu 250\nFred le Pillard 200\njaydeeya 100 (auto)\nWaidmann 100 (auto)\nLepilou 100\nChuck 200\nRumsteck 100 (auto)\nAstellan 50 (auto)\nClément le Destructeur 100 (auto)\nKarel Alchimiste 60 (auto)\nHonorius 666 le Chasseur 85 (auto)";
+  onSubmitTestImportForm() {
+    this.importDatas =
+      "Promesses de dons (Nom suivi de pf, suivi de '(auto)' si vous voulez un report automatique):\n\nMagicHannibal 40 (auto)\nBelli 1er le croquant 40\nbullitt666 30 (auto)\nSamneveu 250\nFred le Pillard 200\njaydeeya 100 (auto)\nWaidmann 100 (auto)\nLepilou 100\nChuck 200\nRumsteck 100 (auto)\nAstellan 50 (auto)\nClément le Destructeur 100 (auto)\nKarel Alchimiste 60 (auto)\nHonorius 666 le Chasseur 85 (auto)";
     this.onSubmitImportForm();
   }
 }
